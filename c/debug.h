ae87402ded85ce0976904a2facb22510e2887035
Chen Yufei <cyfdecyf@gmail.com>
2010-11-26 13:22:22 -0500

    Add debug.h.
    Contains DPRINTF and ASSERT_PRINTF.

---
+++ c/debug.h
@@ -0,0 +1,25 @@
+#ifndef _DEBUG_H
+#define _DEBUG_H
+
+#ifdef DEBUG
+
+#include <stdio.h>
+#include <assert.h>
+
+# define DPRINTF(fmt, ...) \
+    fprintf(stderr, "%s:%d: %s: " fmt "\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
+# define ASSERT_PRINTF(exp, fmt, ...) \
+    do { \
+      if (!(exp)) \
+          fprintf(stderr, "%s:%d: %s: " fmt "\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+      assert((exp)); \
+    } while (0)
+
+#else
+
+# define DPRINTF(fmt, ...)
+# define ASSERT_PRINTF(exp, fmt, ...)
+
+#endif
+
+#endif /* _DEBUG_H */
