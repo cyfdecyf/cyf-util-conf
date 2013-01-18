b4bc20fb8a1010c28b18f79f9e4d53ccd7f534d7
Chen Yufei <cyfdecyf@gmail.com>
2013-01-18 09:53:58 +0800

    [c] Quote find regex in mktag.sh

--- c/mktag.sh
+++ c/mktag.sh
@@ -2,6 +2,7 @@
 
 LISTFILE=cscope.files
 
-find . -maxdepth 1 -type f -regex .*\.[hc] | sed -e 's,^./,,' > $LISTFILE
+#find . -maxdepth 1 -type f -regex '.*\.[hc]' | sed -e 's,^./,,' > $LISTFILE
+find . -type f -regex '.*\.[hc]' | sed -e 's,^./,,' > $LISTFILE
 cscope -b $LISTFILE
 ctags -L $LISTFILE
