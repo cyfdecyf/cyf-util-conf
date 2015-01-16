f0c500fa34976c5683ce39453a616476865bae7e
Chen Yufei <cyfdecyf@gmail.com>
2015-01-16 10:50:35 +0800

    debug.h: always let the compiler see debug code.

--- c/debug.h
+++ c/debug.h
@@ -1,29 +1,26 @@
 #ifndef _DEBUG_H
 #define _DEBUG_H
 
-#ifdef DEBUG
-
 #include <stdio.h>
 #include <assert.h>
 
-# define DPRINTF(fmt, ...) \
-    fprintf(stderr, "%s:%d: %s: " fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
-# define DPRINTLN(fmt, ...) \
-    DPRINTF(fmt "\n", ##__VA_ARGS__)
+/* Always let the compiler see debug code for long term maintenance.
+ * Refer to http://stackoverflow.com/a/1644898/306935 */
 
-# define ASSERT_PRINTF(exp, fmt, ...) \
-    do { \
-      if (!(exp)) \
-          fprintf(stderr, "%s:%d: %s: " fmt "\n", __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
-      assert((exp)); \
-    } while (0)
-
+#ifdef DEBUG
+#define DEBUG_TEST 1
 #else
-
-# define DPRINTF(fmt, ...)
-# define DPRINTLN(fmt, ...)
-# define ASSERT_PRINTF(exp, fmt, ...)
-
+#define DEBUG_TEST 0
 #endif
+
+# define DPRINTF(fmt, ...) \
+    do { if (DEBUG_TEST) fprintf(stderr, "%s:%d: %s: " fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); } while (0)
+
+# define ASSERT_DPRINTF(exp, fmt, ...) \
+    do { \
+        if (DEBUG_TEST && !(exp)) \
+            fprintf(stderr, "%s:%d: %s: " fmt, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+        assert(exp); \
+    } while (0)
 
 #endif /* _DEBUG_H */
